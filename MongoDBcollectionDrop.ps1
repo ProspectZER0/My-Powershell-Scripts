0e05b5bd0443d908a20e363148036b894fc4558d
ProspectZER0 <117657895+ProspectZER0@users.noreply.github.com>
2023-01-12 11:01:37 -0600

    Add files via upload
    Database Collection Purge

---
+++ MongoDBcollectionDrop.ps1
@@ -0,0 +1,25 @@
+﻿#Copying Account Authentication Collection#
+
+#Dropping Account Authentication Collection#
+mongo smartdeploydb --eval 'db.AccountAuthentication.drop()'
+Write-Host "Account Authentication has been dropped."
+
+#Dropping Api Resource Collection#
+mongo smartdeploydb --eval 'db.Id4ApiResourceStore.drop()'
+Write-Host "API Resource has been dropped."
+
+#Dropping Client Store Collection#
+mongo smartdeploydb --eval 'db.Id4ClientStore.drop()'
+Write-Host "Client Store has been dropped."
+
+#Dropping Resource Store Collection#
+mongo smartdeploydb --eval 'db.Id4IdentityResourceStore.drop()'
+Write-Host "Identity Resource Store has been dropped."
+
+#Dropping Persisted Grant Store Collection#
+mongo smartdeploydb --eval 'db.Id4PersistedGrantStore.drop()'
+Write-Host "Persisted Grant Store has been dropped."
+
+#Dropping User Store Collection#
+mongo smartdeploydb --eval 'db.Id4UserStore.drop()'
+Write-Host "User Store has been dropped."
